1b0ede08335a0cbed5431c1e7c1949a96fcb8b6f
MrMerlot <128251550+MrMerlot@users.noreply.github.com>
2023-12-03 00:16:45 -0500

    Update resnet-50.py

--- resnet-50.py
+++ resnet-50.py
@@ -1,16 +1,18 @@
 import torch
+import time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from torch import nn
 from torch import optim
 from torchvision import transforms
-from torchvision.models import resnet50
+from torchvision.models import resnet50, ResNet50_Weights
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import Normalize, Compose
 
 # Pre-processing the images
+# timer
 normalize = Normalize(mean=[0.5, 0.5, 0.5], std=[0.25, 0.25, 0.25])
 data_transforms = {
     'train':
@@ -48,29 +50,29 @@ else:
     print("CPU is available")
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# Initialize resnet model with/without pretrained weights
-model = resnet50(weights=True).to(device)
+# Initialize resnet model with pretrained weights
+model = resnet50(weights=ResNet50_Weights.DEFAULT).to(device)
 
-# Freeze all pretrained layers
+# Unfreeze all layers
 for param in model.parameters():
-    param.requires_grad = False
+    param.requires_grad = True
 
 # Add a dense layer
 model.fc = nn.Sequential(
-    nn.Linear(2048, 128),
+    nn.Linear(2048, 1024),
     nn.ReLU(inplace=True),
-    nn.Linear(128, 525)
+    nn.Linear(1024, 525)
 ).to(device)
 
 # define loss calculator and custom layer optimiser
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.fc.parameters())
-
+lossFn = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
 
 # define the model training fucntion
-def train_model(model, optimizer, criterion, epochCnt):
+def train_model(model, optimizer, lossFn, epochCnt):
 
     for epoch in range(epochCnt):
+        since = time.time()
         print('Epoch: ' + str(epoch+1) + '/' + str(epochCnt))
         print('-' * 10)
 
@@ -86,7 +88,7 @@ def train_model(model, optimizer, criterion, epochCnt):
                 inputs = inputs.to(device)
                 labels = labels.to(device)
                 outputs = model(inputs)
-                loss = criterion(outputs, labels)
+                loss = lossFn(outputs, labels)
 
                 if phase == 'train':
                     optimizer.zero_grad()
@@ -100,17 +102,16 @@ def train_model(model, optimizer, criterion, epochCnt):
             epochLoss = runningLoss / len(image_datasets[phase])
             epochAcc = correct.double() / len(image_datasets[phase])
 
-            print(phase + ' loss: ' + str(epochLoss) + '   acc: ' + str(epochAcc.item()))
+            timeElapsed = time.time() - since
+            print(phase + ' loss: ' + str(epochLoss) + '   acc: ' + str(epochAcc.item()) + 'time elapsed(s): ' + str(timeElapsed))
 
     return model
 
 # train the custom layer, preserving the resnet-50 backbone
-modelTrained = train_model(model, optimizer, criterion, 10)
+modelTrained = train_model(model, optimizer, lossFn, 30)
 
 # save the model weights
-torch.save(modelTrained.state_dict(), 'importedweights')
+torch.save(modelTrained.state_dict(), 'importedweights.pt')
 
 ##################################################################################################################################################################
 
-
-##################################################################################################################################################################

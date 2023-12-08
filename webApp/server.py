4b91ad70bc38e8a6c2612060aba1d8f6ba8763e7
akv0812 <akv5335@psu.edu>
2023-12-07 19:13:36 -0500

    finalized webapp

--- webApp/server.py
+++ webApp/server.py
@@ -1,45 +1,36 @@
 import os
 
-from flask import *  # Import Flask module
+from flask import Flask, render_template, request, send_from_directory
 from flask_cors import CORS
-import json
 from ultralytics import YOLO
 
 app = Flask(__name__)
 CORS(app)
 
-
 @app.route('/result', methods=["GET", "POST"])
 def result():
     try:
-        print(request.files)
         if 'imageInput' not in request.files:
             return 'No imageInput or empty file'
         imageInput = request.files['imageInput']
-
-        print("imageInput:", imageInput)
 
         saved_path = os.path.join("upload_image", imageInput.filename)
         imageInput.save(saved_path)
 
         result = model.predict(saved_path, conf=0.5, save_conf=True)
 
-        resultDict = {"label": result[0].names.get(result[0].probs.top1), "confidence": result[0].probs.top1conf.item()}
+        predicted_label = result[0].names.get(result[0].probs.top1)
+        confidence = result[0].probs.top1conf.item()
 
-        print("result:", result[0].names.get(result[0].probs.top1), result[0].probs.top1conf.item())
+        return render_template('result.html', predicted_label=predicted_label, confidence=confidence)
 
-        resultString = json.dumps(resultDict)
-
-        return resultString
     except Exception as e:
         return f"Error: {str(e)}"
-
 
 @app.route('/', methods=["GET"])
 @app.route('/home.html', methods=["GET"])
 def get_index():
     return send_from_directory('', 'home.html', mimetype='text/html')
-
 
 if __name__ == '__main__':
     model = YOLO('best.pt')
